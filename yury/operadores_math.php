82c071c374fa43cfabdedaca5c1b99d391718942
Yury Silva - BDS DataSolution <ysilva@bdsdatasolution.com.br>
2022-12-19 16:03:09 -0300

    Adicioando novos conhecimentos no mundo do PHP

---
+++ yury/operadores_math.php
@@ -0,0 +1,16 @@
+<?php
+
+$num1 = 10;
+$num2 = 20;
+
+$soma = $num1 + $num2;
+
+echo " A soma entre $num1 e $num2 fica igual a :$soma \n";
+
+$subtracao = $num1 - $num2;
+
+echo " A subtração entre $num1 e $num2 fica igual a :$subtracao \n";
+
+$multiplicacao = $num1 * $num2;
+
+echo "A mutiplicação entre $num1 e $num2 fica igual a :$multiplicacao \n";
--- yury/teste.php
+++ yury/teste.php
@@ -3,4 +3,4 @@
 $te_amo = "te amo";
 
 echo "Minha namorada é a mulher mais linda do universo!\n";
-echo sprintf("Tipo, ela é muito, mas muito gata mesmo!. ", $te_amo);
+echo "Tipo, ela é muito, mas muito gata mesmo!. $te_amo";
