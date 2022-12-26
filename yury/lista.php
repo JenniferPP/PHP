228a6d4352bc7fa72d21d34b7fbcb54cddb7ea5a
Yury Silva - BDS DataSolution <ysilva@bdsdatasolution.com.br>
2022-12-26 16:53:22 -0300

    Modificando item lista.php

--- yury/lista.php
+++ yury/lista.php
@@ -1,11 +1,10 @@
 <?php
 
-$cidade =["Belém","São Paulo","Rio de Janeiro"];
-$cidade2 =array("Belém","São Paulo","Rio de Janeiro","Santarém","Marabá","Cametá","Bragança");
+$cidade =["Belém","São Paulo","Rio de Janeiro","Santarém","Marabá","Cametá","Bragança"];
 
 // echo $cidade2[5] . PHP_EOL;
 // echo count($cidade2);
 
-for ($i=0; $i < count($cidade2);$i++){
-    echo $cidade2[$i] . PHP_EOL;
+for ($i=0; $i < count($cidade);$i++){
+    echo $cidade[$i] . PHP_EOL;
 }
